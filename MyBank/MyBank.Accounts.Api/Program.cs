121599be93b6710b1b6e34bbd040810a3a6406d1
joao <jgabrielsousa@gmail.com>
2020-03-02 09:32:30 -0300

    removing some death code

--- MyBank/MyBank.Accounts.Api/Program.cs
+++ MyBank/MyBank.Accounts.Api/Program.cs
@@ -16,20 +16,7 @@ namespace MyBank.Accounts.Api
     {
         public static void Main(string[] args)
         {
-         
-
-            ////1. Get the IWebHost which will host this application.
-            //var host = CreateWebHostBuilder(args).Build();
-
-            ////2. Find the service layer within our scope.
-            //using (var scope = host.Services.CreateScope())
-            //{
-            //    //3. Get the instance of BoardGamesDBContext in our services layer
-            //    var services = scope.ServiceProvider;
-            //    //4. Call the DataGenerator to create sample data
-            //    DataGenerator.Initialize(services);
-            //}
-
+        
             CreateWebHostBuilder(args).Build().Run();
 
         }
